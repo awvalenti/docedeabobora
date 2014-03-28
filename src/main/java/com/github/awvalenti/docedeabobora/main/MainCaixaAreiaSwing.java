6e6d0bc96da4184fb96336076df8407c2ae172f2
André Willik Valenti <awvalenti@gmail.com>
2014-03-28 17:32:02 -0300

    PainelControle

--- src/main/java/com/github/awvalenti/docedeabobora/main/MainCaixaAreiaSwing.java
+++ src/main/java/com/github/awvalenti/docedeabobora/main/MainCaixaAreiaSwing.java
@@ -2,16 +2,16 @@ package com.github.awvalenti.docedeabobora.main;
 
 import javax.swing.JFrame;
 
-import com.github.awvalenti.docedeabobora.interfaceusuario.swing.componente.PainelRaias;
-import com.github.awvalenti.docedeabobora.interfaceusuario.swing.fabrica.FabricaObjetosSwing;
+import com.github.awvalenti.docedeabobora.interfaceusuario.swing.componente.ControleUmaVelocidade;
+import com.github.awvalenti.docedeabobora.interfaceusuario.swing.componente.PainelControle;
 
 public class MainCaixaAreiaSwing {
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Doce de ab\u00f3bora");
+		JFrame frame = new JFrame("Doce de Ab\u00f3bora");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		frame.add(new PainelRaias(FabricaObjetosSwing.fabricarRaiaAboboras(), FabricaObjetosSwing.fabricarRaiaDoces()));
+		frame.add(new PainelControle(new ControleUmaVelocidade("Agricultor"), new ControleUmaVelocidade("Doceiro"), new ControleUmaVelocidade("Clientela")));
 
 		frame.pack();
 		frame.setVisible(true);
